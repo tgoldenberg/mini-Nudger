f9dfebcf1a4ee2a103548babf83dd995f5ce456e
tgoldenberg <thomasagoldenberg@gmail.com>
2015-08-24 12:31:39 -0400

    successfully seed Mongo database with users, organizations, and tasks

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,24 +1,26 @@
-if (Organizations.find().count() === 0) {
-  Organizations.insert({
+var organizationsData = [
+  {
     name: "Nudger",
     city: "New York City",
     state: "NY",
     country: "United States"
-  });
-
-  Organizations.insert({
+  },
+  {
     name: "Facebook",
     city: "Menlo Park",
     state: "CA",
     country: "United States"
-  });
-
-  Organizations.insert({
+  },
+  {
     name: "Kaplan",
     city: "Fort Lauderdale",
     state: "FL",
     country: "United States"
-  });
+  }
+]
+
+if (Organizations.find().count() === 0) {
+  seedOrganizations(organizationsData);
 }
 
 var taskData = [
@@ -166,16 +168,77 @@ var taskData = [
     organizationId: Organizations.findOne()._id
   }
 ]
-taskData.forEach(function(task) {
-  Tasks.insert({
-    title: task.title,
-    summary: task.summary,
-    assigneeId: null,
-    assignorId: null,
-    status: task.status,
-    importance: task.importance,
-    createdAt: task.createdAt,
-    updatedAt: task.updatedAt,
-    organizationId: task.organizationId
+
+
+if (Tasks.find().count() === 0) {
+  seedTasks(taskData);
+}
+if (Meteor.users.find().count() === 0) {
+  seedUsers();
+}
+
+function seedTasks(taskData) {
+  taskData.forEach(function(task) {
+    Tasks.insert({
+      title: task.title,
+      summary: task.summary,
+      assigneeId: null,
+      assignorId: null,
+      status: task.status,
+      importance: task.importance,
+      createdAt: task.createdAt,
+      updatedAt: task.updatedAt,
+      organizationId: task.organizationId
+    });
   });
-})
+}
+
+function seedOrganizations(organizationsData) {
+  organizationsData.forEach(function(organization) {
+    Organizations.insert({
+      name: organization.name,
+      city: organization.city,
+      state: organization.state,
+      country: organization.country
+    })
+  });
+}
+
+function seedUsers() {
+  for (i=0; i<20; i++) {
+    var user = usersData();
+    Accounts.createUser({
+      email: user.email,
+      password: user.password,
+      profile: {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        position: user.position,
+        assignee: user.assignee,
+        assignor: user.assignor,
+        organizationId: user.organizationId,
+        updatedAt: user.updatedAt
+      },
+      createdAt: user.createdAt
+    });
+  }
+}
+
+function randomBool() {
+  return Math.random()<.5;
+}
+
+function usersData() {
+  return {
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email(),
+    password: 'password',
+    position: faker.commerce.department(),
+    assignee: randomBool(),
+    assignor: randomBool(),
+    organizationId: Organizations.findOne()._id,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  };
+}
