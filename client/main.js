049a1e8aeeb833e4dc10860cbd04b99b88667b68
tgoldenberg <thomasagoldenberg@gmail.com>
2015-08-24 15:12:01 -0400

    create user registration form on landing page and style

--- client/main.js
+++ client/main.js
@@ -18,4 +18,38 @@ if (Meteor.isClient) {
       Meteor.logout();
     }
   });
+
+  Template.landingContent.helpers({
+    organizationOptions: function() {
+      return Organizations.find();
+    }
+  });
+
+  Template.landingContent.events({
+    'submit .new-user-registration': function(event) {
+      event.preventDefault();
+      console.log("submit signup", event);
+      var firstName = event.target.registerFirstName.value;
+      var lastName = event.target.registerLastName.value;
+      var email = event.target.registerEmail.value;
+      var password = event.target.registerPassword.value;
+      var organization = event.target.registerOrganization.value;
+      var organizationId = Organizations.findOne({name: organization})._id;
+      var position = event.target.registerPosition.value;
+      var assignee = event.target.registerAssignee.value;
+      var assignor = event.target.registerAssignor.value;
+      Accounts.createUser({
+        email: email,
+        password: password,
+        profile: {
+          firstName: firstName,
+          lastName: lastName,
+          organizationId: organizationId,
+          position: position,
+          assignee: assignee,
+          assignor: assignor
+        }
+      });
+    }
+  })
 }
