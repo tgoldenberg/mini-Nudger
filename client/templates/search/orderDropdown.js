9a155ab5c08320401ab3320885f28a50715c8674
tgoldenberg <thomasagoldenberg@gmail.com>
2015-08-25 13:31:01 -0400

    fix issues with modal and remove inserted text after submission

--- client/templates/search/orderDropdown.js
+++ client/templates/search/orderDropdown.js
@@ -2,7 +2,7 @@
 Template.orderDropdown.events({
   'click .order-dropdown-option': function(event) {
     var order = $(event.target).attr("id");
-    $('#order').html(order.split(" ")[0]);
+    $('#order').html(capitalize(order.split(" ")[0]));
     var orderValue = order == "ascending" ? 1 : -1;
     Session.set('order', orderValue);
   }
